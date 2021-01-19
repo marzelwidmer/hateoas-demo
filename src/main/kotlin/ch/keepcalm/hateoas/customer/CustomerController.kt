8c3b329636fe1b5d69e6ad83f6606c13f4c157c9
marzelwidmer <marzelwidmer@gmail.com>
2021-01-19 21:18:43 +0100

    Fix Affordance

--- src/main/kotlin/ch/keepcalm/hateoas/customer/CustomerController.kt
+++ src/main/kotlin/ch/keepcalm/hateoas/customer/CustomerController.kt
@@ -1,13 +1,13 @@
 package ch.keepcalm.hateoas.customer
 
-import org.springframework.hateoas.Affordance
-import org.springframework.hateoas.CollectionModel
-import org.springframework.hateoas.EntityModel
-import org.springframework.hateoas.Link
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
+import org.springframework.hateoas.*
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
 import org.springframework.hateoas.server.mvc.afford
 import org.springframework.web.bind.annotation.*
+import org.springframework.http.HttpEntity
+import org.springframework.http.ResponseEntity
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder
 
 @RestController
 class CustomerController(private val customerService: CustomerService) {
@@ -17,24 +17,40 @@ class CustomerController(private val customerService: CustomerService) {
         val selfLink: Link = linkTo(
             methodOn(CustomerController::class.java).one(id)
         ).withSelfRel()
-
-        val update : Affordance = afford<Any> {
-            methodOn(CustomerController::class.java).update(customer = Customer(firstName = "", lastName = ""), id)
+        val create: Affordance = afford<CustomerController> {
+            methodOn(CustomerController::class.java).add(customer = Customer(firstName = "", lastName = ""))
         }
-//        val aggregateRoot : Link = linkTo(methodOn(CustomerController::class.java))
-        return EntityModel.of(customerService.getCustomer(id) as Customer, selfLink)
+        val aggregateRoot : Link = linkTo(methodOn(CustomerController::class.java).all()).withRel("customers")
+            .andAffordance(WebMvcLinkBuilder.afford(methodOn(CustomerController::class.java).add(customer = Customer(firstName = "", lastName = ""))))
+
+        return EntityModel.of(customerService.getCustomer(id) as Customer, selfLink.andAffordance(create), aggregateRoot)
     }
 
-    @PutMapping(value = ["/api/customers/{id}"])
-    fun update(@RequestBody customer: Customer, @PathVariable id: Int) {
-        println("Update")
-    }
 
     @GetMapping(value = ["/api/customers"])
     fun all(): CollectionModel<Customer> {
         val selfLink: Link = linkTo(
             methodOn(CustomerController::class.java).all()
         ).withSelfRel()
-        return  CollectionModel.of(  customerService.getCustomers(), selfLink )
+        return CollectionModel.of(customerService.getCustomers(), selfLink)
     }
+
+
+    @PostMapping(value = ["/api/customers"])
+    fun add(@RequestBody customer: Customer): HttpEntity<*> {
+        println("Add")
+        println(customer)
+        return  ResponseEntity.EMPTY
+    }
+
+//    @GetMapping("/employees/{id}")
+//    fun findOne(@PathVariable id: Int): EntityModel<Customer> {
+//        val selfLink: Link = linkTo(methodOn(CustomerController::class.java).one(id)).withSelfRel()
+//        val affordance: Link = Affordances.of(linkTo(methodOn(CustomerController::class.java).add(Customer(firstName = "", lastName = "") )).withRel(IanaLinkRelations.CREATE_FORM_VALUE))
+//            .afford(HttpMethod.POST)
+//            .withInput(Customer::class.java)
+//            .withName(IanaLinkRelations.CREATE_FORM_VALUE)
+//            .toLink()
+//        return EntityModel.of(customerService.getCustomer(id) as Customer, selfLink, affordance)
+//    }
 }
