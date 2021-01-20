57b115874f07ac0a9540430b6a9261d52e516d24
marzelwidmer <marzelwidmer@gmail.com>
2021-01-20 16:37:31 +0100

    Save

--- src/main/kotlin/ch/keepcalm/hateoas/HateoasDemoApplication.kt
+++ src/main/kotlin/ch/keepcalm/hateoas/HateoasDemoApplication.kt
@@ -2,12 +2,6 @@ package ch.keepcalm.hateoas
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.hateoas.*
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class HateoasDemoApplication
