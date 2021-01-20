fbc5558fb5b0083136edc874907d4296a92128e5
marzelwidmer <marzelwidmer@gmail.com>
2021-01-20 17:33:56 +0100

    Add Description (ALPS)

--- src/main/kotlin/ch/keepcalm/hateoas/message/Message.kt
+++ src/main/kotlin/ch/keepcalm/hateoas/message/Message.kt
@@ -2,6 +2,7 @@ package ch.keepcalm.hateoas.message
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.rest.core.annotation.Description
 
 @Table(value = "MESSAGES")
-data class Message(@Id val id: String?, val text: String)
+data class Message(@Id val id: String?,  @Description(value = "Just a custom Message") val text: String)
